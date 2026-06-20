c7c0eee10ad64f625e8a5e3ab39b1eb3e3f70e19
Tayyba Sattar <tayybasattar1@gmail.com>
2026-06-20 14:28:36 +0500

    Add files via upload
    This is my initial version of a GPA calculator.

---
+++ gpa_calculator.py
@@ -0,0 +1,7 @@
+print("Hey! I am a GPA Calculator!")
+print("Let's get started!")
+courses = int(input("Enter the number of your courses: "))
+chs = float(input("Enter your credit hours: "))
+gradepoints = float(input("Enter your gradepoints: "))
+gpa = gradepoints/chs
+print("Your GPA is",gpa)
